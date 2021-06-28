ab1791fa74bb5a9a9a87207a9f03d92fb3e43fd5
TanvirHasanAshik <tanvirashik41@gmail.com>
2021-06-28 12:27:13 +0600

    node-mongo practice start

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send("Thanks to learning node js");
+});
+app.listen(3000, console.log('listening port 3000'));
